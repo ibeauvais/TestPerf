3dea0bcefdb3623c993c0c27280329c50d26926c
MikaSez <msadovnikov@xebia.frx>
2018-01-04 02:26:58 +0100

    Ajout du random

--- src/test/java/TestUtils.java
+++ src/test/java/TestUtils.java
@@ -4,24 +4,50 @@ import java.util.stream.IntStream;
 
 public class TestUtils {
 
-    private static Random random = new Random();
+    private static int[] array = new Random().ints(50_000, 50_000, 100_000).toArray();
 
-    public static void testPerformanceMilli(Function<int[], String> fonctionToTest, String prefix) {
+    public static int[] randomGenerator() {
+        return new Random().ints(50_000, 50_000, 100_000).toArray();
+    }
+
+    public static void testPerformanceMilli(Function<int[], String> functionToTest, String prefix) {
         long time = System.currentTimeMillis();
-        int[] data = getData();
-
-        fonctionToTest.apply(data);
+        functionToTest.apply(array);
         long end = System.currentTimeMillis() - time;
         System.out.println("duration of " + prefix + end);
     }
 
-    public static void testPerformanceNano(Function<int[], String> fonctionToTest, String prefix) {
-        long nanoTime = System.nanoTime();
-        int[] data = getData();
+    public static void randomTestPerformanceMilli(Function<int[], String> functionToTest, String prefix) {
+        int[] data = randomGenerator();
+        long time = System.currentTimeMillis();
 
-        fonctionToTest.apply(data);
+        functionToTest.apply(data);
+        long end = System.currentTimeMillis() - time;
+        System.out.println("duration of " + prefix + end);
+    }
+
+
+    public static void testPerformanceNano(Function<int[], String> functionToTest, String prefix) {
+        long nanoTime = System.nanoTime();
+        functionToTest.apply(array);
         long end = System.nanoTime() - nanoTime;
         System.out.println("duration of " + prefix + end);
+    }
+
+
+    public static void randomTestPerformanceNano(Function<int[], String> functionToTest, String prefix) {
+        int[] data = randomGenerator();
+        long nanoTime = System.nanoTime();
+        functionToTest.apply(data);
+        long end = System.nanoTime() - nanoTime;
+        System.out.println("duration of " + prefix + end);
+    }
+
+    public static void warmUp(Function<int[], String> functionToTest) {
+        for (int i = 0; i < 1_000; i++) {
+            int[] data = array;
+            functionToTest.apply(randomGenerator());
+        }
     }
 
 
---
+++ src/test/java/TestWithWarmupData.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+
+public class TestWithWarmupData {
+
+
+    @Test
+    public void test_milli_performance_implementation_1() {
+        TestUtils.warmUp(NumberUtils::transformNumbers1);
+        TestUtils.testPerformanceMilli(NumberUtils::transformNumbers1, "Lambda millitest : ");
+    }
+
+    @Test
+    public void test_milli_performance_implementation_2() {
+        TestUtils.warmUp(NumberUtils::transformNumbers2);
+        TestUtils.testPerformanceMilli(NumberUtils::transformNumbers2, "for/stringbuilder milliTest : ");
+    }
+
+    @Test
+    public void test_nano_performance_implementation_1() {
+        TestUtils.warmUp(NumberUtils::transformNumbers1);
+        TestUtils.testPerformanceNano(NumberUtils::transformNumbers1, "Lambda nanoTest : ");
+    }
+
+    @Test
+    public void test_nano_performance_implementation_2() {
+        TestUtils.warmUp(NumberUtils::transformNumbers2);
+        TestUtils.testPerformanceNano(NumberUtils::transformNumbers2, "for/stringbuilder nanoTest : ");
+    }
+
+
+    @Test
+    public void random_test_milli_performance_implementation_1() {
+        TestUtils.warmUp(NumberUtils::transformNumbers1);
+        TestUtils.randomTestPerformanceMilli(NumberUtils::transformNumbers1, "Lambda millitest random : ");
+    }
+
+    @Test
+    public void random_test_milli_performance_implementation_2() {
+        TestUtils.warmUp(NumberUtils::transformNumbers2);
+        TestUtils.randomTestPerformanceMilli(NumberUtils::transformNumbers2, "for/stringbuilder milliTest  random : ");
+    }
+
+    @Test
+    public void random_test_nano_performance_implementation_1() {
+        TestUtils.warmUp(NumberUtils::transformNumbers1);
+        TestUtils.randomTestPerformanceNano(NumberUtils::transformNumbers1, "Lambda nanoTest  random : ");
+    }
+
+    @Test
+    public void random_test_nano_performance_implementation_2() {
+        TestUtils.warmUp(NumberUtils::transformNumbers2);
+        TestUtils.randomTestPerformanceNano(NumberUtils::transformNumbers2, "for/stringbuilder nanoTest  random : ");
+    }
+
+
+}
