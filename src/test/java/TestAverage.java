209360199262207830e7352d5971b4da578620c0
MikaSez <msadovnikov@xebia.frx>
2018-01-08 10:45:38 +0100

    Ajout d'un runner parametré

--- src/test/java/TestAverage.java
+++ src/test/java/TestAverage.java
@@ -1,18 +1,36 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TestAverage {
 
+    @Parameter
+    public int iterationCount;
+
+    @Parameters
+    public static Collection<Integer> iterations() {
+        Integer[] values = {10, 100, 1000, 10000};
+        return Arrays.asList(values);
+    }
+
 
     @Test
-    public void test_nano_performance_implementation_1() {
+    public void test_nano_performance_implementation_1_10() {
         TestUtils.warmUp(NumberUtils::transformNumbers1);
-        TestUtils.testAveragePerformanceNano(NumberUtils::transformNumbers1, "Lambda nanoTest : ", 1000);
+        TestUtils.testAveragePerformanceNano(NumberUtils::transformNumbers1, "Lambda nanoTest " + iterationCount + " : ", iterationCount);
     }
 
     @Test
-    public void test_nano_performance_implementation_2() {
+    public void test_nano_performance_implementation_2_10() {
         TestUtils.warmUp(NumberUtils::transformNumbers2);
-        TestUtils.testAveragePerformanceNano(NumberUtils::transformNumbers2, "for/stringbuilder nanoTest : ", 1000);
+        TestUtils.testAveragePerformanceNano(NumberUtils::transformNumbers2, "for/stringbuilder nanoTest " + iterationCount + " : ", iterationCount);
     }
+
 
 }
